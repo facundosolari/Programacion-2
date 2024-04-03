e7f962cdf8b7c5f4e2148fbbe6b585066becb76c
Facundo Solari <facusolari9@gmail.com>
2024-04-03 16:57:55 -0300

    initial commit

---
+++ exercise2.py
@@ -0,0 +1,70 @@
+"""Lógica Simple y Cortocircuito"""
+
+
+"""
+Construir una expresión lógica que use TODAS las variables y cuyo resultado sea
+True si al menos una de las variables es True.
+"""
+
+esta_lloviendo = True
+riego_activado = True
+
+# COMPLETAR - INICIO
+piso_mojado = (esta_lloviendo or riego_activado)
+
+# COMPLETAR - FIN
+
+assert piso_mojado
+
+
+"""
+Construir una expresión lógica que use TODAS las variables y cuyo resultado sea
+True si el área es mayor a 5.
+Restricción: Usar NOT.
+"""
+
+lado_cuadrado = 5
+area_cuadrado = pow(lado_cuadrado, 2)
+
+# COMPLETAR - INICIO
+area_mayor_a_cinco = not (lado_cuadrado > area_cuadrado)
+
+# COMPLETAR - FIN
+
+assert area_mayor_a_cinco
+
+
+"""
+Construir una expresión lógica que use TODAS las variables y cuyo resultado sea
+True si el número 1 es divisible por 7 y al mismo tiempo el número 2 no lo es.
+"""
+
+numero_1 = 49
+numero_2 = 50
+
+# COMPLETAR - INICIO
+resultado = (numero_1 % 7 == 0) & (numero_2 % 7 != 0)
+
+# COMPLETAR - FIN
+
+assert resultado
+
+
+"""
+Construir una expresión lógica que use TODAS las variables y cuyo resultado sea
+el mismo valor de la variable 3.
+Restricción: sólo usar OR, NOT y el mecanismo de cortocircuito.
+"""
+
+variable_01 = False
+variable_02 = True
+variable_03 = 80
+variable_04 = "90"
+variable_05 = 100
+
+# COMPLETAR - INICIO
+resultado = not (variable_01 or variable_05) or not variable_02 or not variable_04 or variable_03
+
+# COMPLETAR - FIN
+
+assert resultado == 80
---
+++ exercise4.py
@@ -0,0 +1,82 @@
+"""Conversiones Básicas"""
+
+
+"""
+Convertir los numeros de string a enteros y luego sumarlos.
+"""
+
+numero_01 = "123"
+numero_02 = "456"
+numero_03 = "789"
+numero_04 = "132"
+
+# COMPLETAR - INICIO
+numero_01 = int(numero_01)
+numero_02 = int(numero_02)
+numero_03 = int(numero_03)
+numero_04 = int(numero_04)
+suma_de_numeros = numero_01 + numero_02 + numero_03 + numero_04
+
+# COMPLETAR - FIN
+
+assert suma_de_numeros == 1500
+
+
+"""
+Convertir los numeros de enteros a string y luego concatenarlos.
+"""
+
+numero_01 = 123
+numero_02 = 456
+numero_03 = 789
+
+# COMPLETAR - INICIO
+numero_01 = str("123")
+numero_02 = str("456")
+numero_03 = str("789")
+suma_de_numeros_string = numero_01 + numero_02 + numero_03
+
+# COMPLETAR - FIN
+
+assert suma_de_numeros_string == "123456789"
+
+
+"""
+Convertir los numeros de binario, octal y hexadecimal a enteros y luego
+multiplicarlos.
+"""
+
+numero_binario = "0b111010110101110111101000000"
+numero_octal = "0o1425"
+numero_hexadecimal = "0x6f540"
+
+# COMPLETAR - INICIO
+numero_binario = int(numero_binario,2)
+numero_octal= int(numero_octal,8)
+numero_hexadecimal = int(numero_hexadecimal,16)
+multiplicacion_de_numeros = numero_binario * numero_octal * numero_hexadecimal
+
+# COMPLETAR - FIN
+
+assert multiplicacion_de_numeros == 44397345600000000
+
+
+"""
+Convertir todo los numeros a enteros y luego restarlos secuencialmente (El uno
+menos el dos menos el tres menos el cuatro).
+"""
+
+numero_01 = "987"
+numero_02 = "0x6f54F"
+numero_03 = "0o1234"
+numero_04 = 654
+
+# COMPLETAR - INICIO
+numero_01 = int(numero_01)
+numero_02 = int(numero_02,16)
+numero_03 = int(numero_03,8)
+resultado_resta = (((numero_01 - numero_02) - numero_03) - numero_04)
+
+# COMPLETAR - FIN
+
+assert resultado_resta == -456350
